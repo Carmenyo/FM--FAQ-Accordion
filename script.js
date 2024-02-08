4282ade788863b5cc0ac19aacc07c3f86595615f
Carmen Castro <carmencastro@MacBook-Air-de-Carmen.local>
2024-02-08 17:49:52 +0100

    starting in minus icon

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const headings = document.querySelectorAll('.faq__heading');
-  
-    headings.forEach(heading => {
-      heading.addEventListener('click', function () {
-        const item = heading.closest('.faq__item');
-        const answer = item.querySelector('.faq__answer');
-  
-        answer.classList.toggle('faq__answer--visible');
-  plusIcon.classList.toggle('icon--hidden', answer.classList.contains('faq__answer--visible'));
-      minusIcon.classList.toggle('icon--hidden', !answer.classList.contains('faq__answer--visible'));
-        const plusIcon = heading.querySelector('.icon-plus');
-        const minusIcon = heading.querySelector('.icon-minus');
+  const headings = document.querySelectorAll(".faq__heading");
 
+  headings.forEach((heading, index) => {
+    const item = heading.closest(".faq__item");
+    const answer = item.querySelector(".faq__answer");
+    const plusIcon = heading.querySelector(".icon-plus");
+    const minusIcon = heading.querySelector(".icon-minus");
 
-        // Muestra el ícono "plus" si la respuesta está oculta, y "minus" si la respuesta está visible
-        plusIcon.classList.toggle('icon--hidden', answer.classList.contains('faq__answer--visible'));
-        minusIcon.classList.toggle('icon--hidden', !answer.classList.contains('faq__answer--visible'));
-      });
-  
-      // Oculta inicialmente el ícono "minus" en cada pregunta
-      const minusIcon = heading.querySelector('.icon-minus');
-      minusIcon.classList.add('icon--hidden');
+    // Muestra la primera pregunta desplegada con el icono "menos"
+    if (index === 0) {
+      answer.classList.add("faq__answer--visible");
+      minusIcon.classList.remove("icon--hidden");
+      plusIcon.classList.add("icon--hidden");
+    }
+
+    heading.addEventListener("click", function () {
+      answer.classList.toggle("faq__answer--visible");
+      plusIcon.classList.toggle(
+        "icon--hidden",
+        answer.classList.contains("faq__answer--visible")
+      );
+      minusIcon.classList.toggle(
+        "icon--hidden",
+        !answer.classList.contains("faq__answer--visible")
+      );
     });
+
+    // Oculta inicialmente el ícono "minus" en cada pregunta excepto la primera
+    if (index !== 0) {
+      minusIcon.classList.add("icon--hidden");
+    }
   });
-  
+});
